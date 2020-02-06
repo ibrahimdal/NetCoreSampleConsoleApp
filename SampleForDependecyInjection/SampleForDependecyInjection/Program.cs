4c9cc943f2f6494119fac474ece3b201a43eed36
ibrahimdal <ibrahimdal999@gmail.com>
2020-02-06 22:36:13 +0300

    Init.

---
+++ SampleForDependecyInjection/SampleForDependecyInjection/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SampleForDependecyInjection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
